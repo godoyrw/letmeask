b385e694ba393e76a1e551e3b7d3e85012a1dcd8
Roberto Godoy <godoyrw@ail.com>
2021-06-22 03:36:55 -0300

    Initial Commit

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import './services/firebase';
+
+import './assets/styles/global.scss';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+//https://docs.google.com/forms/d/e/1FAIpQLSdUK44aYYlvAAoJEG1PXZDGwzqkrJSopt8yRnNbhfuuhJuyCA/viewform
+/**
+ * Apoiar minha Squad de Devs - Sou CTO e gosta de ser um líder presente, ajudar, ensinar e aprender, tudo isso em foco na evolução de toda a Squad!
+ */
